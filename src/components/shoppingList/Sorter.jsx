6fb8b3430c757d4ec7a93ad09a946feb70a590f7
Sylvia Tadross <sylvia.adel.emil@gmail.com>
2023-02-09 21:19:46 +0100

    add sorter component

---
+++ src/components/shoppingList/Sorter.jsx
@@ -0,0 +1,9 @@
+export default function Sorter() {
+  return (
+    <section className="sorter">
+      Sort by:
+      <button className="link-btn">Name</button>
+      <button className="link-btn">Price</button>
+    </section>
+  );
+}
--- src/screens/ShoppingList.jsx
+++ src/screens/ShoppingList.jsx
@@ -1,9 +1,11 @@
+import Sorter from "../components/shoppingList/Sorter";
 import eyeOpenIcon from "../assets/images/icon-eye-open.svg";
 
 export default function ShoppingListing() {
   return (
     <div id="shopping-list">
       <h1>Shopping List</h1>
+      <Sorter />
       <button className="primary-btn">Add item</button>
       <button className="link-btn">
         <img src={eyeOpenIcon} />
