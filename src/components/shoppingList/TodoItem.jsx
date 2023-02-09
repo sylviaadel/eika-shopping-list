dab4fa818153b244f8494f0db086cd813bbe633a
Sylvia Tadross <sylvia.adel.emil@gmail.com>
2023-02-09 21:45:16 +0100

    Add todoList component

---
+++ src/components/shoppingList/TodoItem.jsx
@@ -0,0 +1,10 @@
+export default function TodoItem() {
+  return (
+    <article className="todo-item">
+      <input type="checkbox" />
+      <span className="item-name">Chair</span>
+      <span className="spacer"></span>
+      <span className="item-price">300:-</span>
+    </article>
+  );
+}
---
+++ src/components/shoppingList/TodoList.jsx
@@ -0,0 +1,10 @@
+import TodoItem from "./TodoItem";
+
+export default function TodoList() {
+  return (
+    <section>
+      <TodoItem />
+      <p>No items in this list</p>
+    </section>
+  );
+}
