b540cb874309adc2183a1cfc9c9aac2f817bc51d
Sylvia Tadross <sylvia.adel.emil@gmail.com>
2023-02-13 21:37:36 +0100

    Add styles and add validation to modal

--- src/components/modal/AddItemForm.jsx
+++ src/components/modal/AddItemForm.jsx
@@ -3,25 +3,38 @@ import { useState } from "react";
 export default function Form() {
   const [name, setName] = useState("");
   const [price, setPrice] = useState("");
+  const nameError = <p className="error-text">Please enter a valid name.</p>;
+  const priceError = <p className="error-text">Please enter a valid price.</p>;
+
+  function validate() {
+    setName(name.trim());
+  }
+
+  function onSubmit(e) {
+    if (nameError || priceError) {
+      e.preventDefault();
+    }
+  }
 
   return (
-    <form>
+    <form onSubmit={onSubmit}>
       <label>Product name</label>
       <input
         autoFocus
         type="text"
-        required
         value={name}
         onChange={(e) => setName(e.target.value)}
+        onBlur={() => validate()}
       />
+      {name.length < 1 && nameError}
       <label>Price</label>
       <input
         type="number"
-        required
         value={price}
-        min={1}
         onChange={(e) => setPrice(e.target.value)}
+        onBlur={() => validate()}
       />
+      {price < 1 && priceError}
       <button className="primary-btn" type="submit">
         Submit
       </button>
