00fbf539a3d4b2d6809bdbd0f4672b4fa361658b
Sylvia Tadross <sylvia.adel.emil@gmail.com>
2023-02-17 14:57:42 +0100

    Add sorter functions to component

--- src/components/shoppingList/Sorter.jsx
+++ src/components/shoppingList/Sorter.jsx
@@ -1,23 +1,25 @@
 import { useItems } from "../../state/ItemsContext";
+import { sortByName, sortByPrice } from "../../scripts/tests/sorter";
 
 export default function Sorter() {
   const { items, setItems } = useItems();
-  const clonedItems = [...items];
-  function sortByName() {
-    const sortedList = clonedItems.sort((a, b) => (a.name > b.name ? 1 : -1));
-    setItems(sortedList);
+  function sortName() {
+    const clonedItems = [...items];
+    sortByName(clonedItems);
+    setItems(clonedItems);
   }
-  function sortByPrice() {
-    const sortedList = clonedItems.sort(({ price: a }, { price: b }) => a - b);
-    setItems(sortedList);
+  function sortPrice() {
+    const clonedItems = [...items];
+    sortByPrice(clonedItems);
+    setItems(clonedItems);
   }
   return (
     <section className="sorter">
       Sort by:
-      <button className="link-btn" onClick={sortByName}>
+      <button className="link-btn" onClick={sortName}>
         Name
       </button>
-      <button className="link-btn" onClick={sortByPrice}>
+      <button className="link-btn" onClick={sortPrice}>
         Price
       </button>
     </section>
