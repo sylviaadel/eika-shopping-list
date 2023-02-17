365a21539f92f63d1e910c6b7503e3558caff569
Sylvia Tadross <sylvia.adel.emil@gmail.com>
2023-02-17 18:03:16 +0100

    change function name

--- src/scripts/tests/addItem.js
+++ src/scripts/tests/addItem.js
@@ -1,10 +1,10 @@
-export function addItemName(name) {
+export function validItemName(name) {
   if (name === undefined || name === null || name.trim() === "") {
     return false;
   }
   return true;
 }
-export function addItemPrice(price) {
+export function validItemPrice(price) {
   if (price === null || price < 1) {
     return false;
   }
